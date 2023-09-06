0b7a86f0918cefc7ac23493604d3f488861f0f6a
mattgon1989 <mattgon50@gmail.com>
2023-09-06 16:42:04 -0700

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,21 @@
+
+//   create a message for the user describing the vault and the combination
+//   calculate the first combination number by adding 5 and 5
+//   calculate the second combination number by multiplying 4 and 10
+//   calculate the third combination number by subtracting 40 and 1
+//   show the message and the vault combination to the user
+
+// create message for user
+const messageForUser = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+
+// calculate first combination number
+const num1 = 5 + 5;
+
+// calculate second combination number
+const num2 = 4 * 10;
+
+// calculate third combination number
+const num3 = 40 - 1;
+
+// create alert dialog with message and vault combination
+alert(messageForUser + ' ' + num1 + ' ' + num2 + ' ' + num3);
